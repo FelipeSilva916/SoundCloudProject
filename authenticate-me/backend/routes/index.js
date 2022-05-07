3e9e1062105208eb2a6989906d808e2394c066af
Felipe Silva <felipesilva916@gmail.com>
2022-05-07 15:21:14 -0700

    Created user model

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const apiRouter = require("express").Router();
+const apiRouter = require("./api");
 //
 router.use("/api", apiRouter);
 //
