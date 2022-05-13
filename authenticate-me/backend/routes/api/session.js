86420c0bc0096c26530e24ec90145029f38cc4e6
Felipe Silva <felipesilva916@gmail.com>
2022-05-12 20:08:10 -0700

    Finished phase 5

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -2,10 +2,25 @@
 const express = require("express");
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
-
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 const router = express.Router();
-// Log in
-router.post("/", async (req, res, next) => {
+
+//=======================================//
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors
+];
+//
+
+// =============  Log in =================//
+router.post("/", validateLogin, async (req, res, next) => {
   const { credential, password } = req.body;
 
   const user = await User.login({ credential, password });
@@ -32,6 +47,9 @@ router.delete("/", (_req, res) => {
   res.clearCookie("token");
   return res.json({ message: "success" });
 });
+//
+//
+
 //
 //
 module.exports = router;
