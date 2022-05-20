fbd098b11dc99fed6885987d71f3a1dff0b0756d
Felipe Silva <felipesilva916@gmail.com>
2022-05-20 12:22:19 -0700

    finished get all albums

--- backend/routes/api/albums.js
+++ backend/routes/api/albums.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { Album } = require("../../db/models");
 
 router.get("/test", (req, res, next) => {
   res.json("Albums go here");
+});
+
+// =============== GET All Albums =============== //
+router.get("/albums", async (req, res) => {
+  const allAlbums = await Album.findAll();
+  res.json({ allAlbums });
 });
 
 module.exports = router;
