a2b4ded672dae72832c0992cc79c6b68f292076d
Felipe Silva <felipesilva916@gmail.com>
2022-05-20 10:41:26 -0700

    updated

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const { check } = require("express-validator");
 const { Song, User, Album } = require("../../db/models");
 
+const {
+  setTokenCookie,
+  requireAuth,
+  restoreUser
+} = require("../../utils/auth");
+
 router.get("/testsongs", (req, res, next) => {
   res.json("Songs go here");
 });
 
-// ======== Get songs by ID =======//
+// ============== Get songs by ID ==================//
 router.get("/songs/:songId", async (req, res, next) => {
   const { songId } = req.params;
   const song = await Song.findByPk(songId, {
@@ -26,7 +32,22 @@ router.get("/songs/:songId", async (req, res, next) => {
   return res.json(song);
 });
 
-// ====== Get all songs ===========//
+//=============== Delete Song ======================//
+router.delete("/songs/:songId", requireAuth, async (req, res) => {
+  const { user } = req;
+  const { songId } = req.params;
+
+  const deletedSong = await Song.findByPk(songId);
+
+  if (deletedSong) {
+    if (deletedSong.userId === user.id) {
+      await deletedSong.destroy();
+      res.json({ message: "Successfully deleted" });
+    }
+  }
+});
+
+// ================= Get all songs ================//
 router.get("/songs", async (req, res) => {
   const songs = await Song.findAll();
   res.json(songs);
