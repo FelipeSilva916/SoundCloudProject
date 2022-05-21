1d638b996e1c78acfad37ad58d7893a5aa0c7628
Felipe <87789520+FelipeSilva916@users.noreply.github.com>
2022-05-20 19:31:15 -0700

    finished create album

--- backend/routes/api/albums.js
+++ backend/routes/api/albums.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { Album } = require("../../db/models");
+const { handleValidationErrors } = require("../../utils/validation");
+const {
+  setTokenCookie,
+  requireAuth,
+  restoreUser
+} = require("../../utils/auth");
+const { User, Song, Album } = require("../../db/models");
+const { jwtConfig } = require("../../config");
 
 router.get("/test", (req, res, next) => {
   res.json("Albums go here");
@@ -10,6 +17,20 @@ router.get("/test", (req, res, next) => {
 router.get("/albums", async (req, res) => {
   const allAlbums = await Album.findAll();
   res.json({ allAlbums });
+});
+
+// ================== Create a new Album ====================//
+router.post("/albums", requireAuth, restoreUser, async (req, res) => {
+  const { user } = req;
+  const { title, description, previewImg } = req.body;
+  const newAlbum = await Album.create({
+    userId: user.id,
+    title,
+    description,
+    previewImg
+  });
+  res.status(201);
+  res.json(newAlbum);
 });
 
 module.exports = router;
