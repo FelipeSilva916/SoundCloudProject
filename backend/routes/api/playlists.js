ad2e9e5aec47144c312613a2a65f1abbb9e3c70d
Felipe <87789520+FelipeSilva916@users.noreply.github.com>
2022-05-24 10:43:58 -0700

    finished create playlist

--- backend/routes/api/playlists.js
+++ backend/routes/api/playlists.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const {
+  handleValidationErrors,
+  validatePlaylist
+} = require("../../utils/validation");
+const {
+  setTokenCookie,
+  requireAuth,
+  restoreUser
+} = require("../../utils/auth");
+const { User, Song, Album, Playlist } = require("../../db/models");
 
-router.get("/testplaylists", (req, res, next) => {
-  res.json("playlists go here");
-});
+//====== Create a Playlist ========//
+router.post(
+  "/playlists",
+  requireAuth,
+  validatePlaylist,
+  async (req, res, next) => {
+    const { user } = req;
+    const { name, imageUrl } = req.body;
+
+    const newPlaylist = await Playlist.create({
+      userId: user.id,
+      name: name,
+      previewImg: imageUrl
+    });
+    res.status(201);
+    res.json(newPlaylist);
+  }
+);
 
 module.exports = router;
