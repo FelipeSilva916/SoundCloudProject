8cc83b3f1ca487aa2b1fa2c71e69ff70a7b670cb
Felipe Silva <felipesilva916@gmail.com>
2022-06-22 19:35:44 -0700

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
-import './index.css';
-import App from './App';
-import configureStore from './store';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
@@ -26,5 +30,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
