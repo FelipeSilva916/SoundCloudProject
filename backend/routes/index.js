3ce18dc9576febd4805734655cec8c919c505d37
Felipe Silva <felipesilva916@gmail.com>
2022-06-22 19:31:42 -0700

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -18,6 +18,31 @@ router.use("/playlists", playlistsRouter);
 router.use("/my", myRouter);
 router.use("/artists", artistRouter);
 router.use(userRouter);
+
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  // Serve the frontend's index.html file at the root route
+  router.get("/", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
+  });
+}
+
 //
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
