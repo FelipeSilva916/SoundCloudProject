e6c50858e160f5a96457f84a18e1805d01f17ebe
Felipe Silva <felipesilva916@gmail.com>
2022-06-23 21:08:01 -0700

    Update session.js

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -73,3 +73,11 @@ export const signup = (user) => async (dispatch) => {
   dispatch(setUser(data));
   return response;
 };
+
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session", {
+    method: "DELETE"
+  });
+  dispatch(removeUser());
+  return response;
+};
