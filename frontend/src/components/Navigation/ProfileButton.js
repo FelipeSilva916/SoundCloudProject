e17fec209aad6cd9d1df5f909abc2dc9d6a9e6e7
Felipe Silva <felipesilva916@gmail.com>
2022-06-30 15:27:50 -0700

    fixed log out

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
+  const history = useHistory();
   const [showMenu, setShowMenu] = useState(false);
 
   const openMenu = () => {
@@ -26,6 +28,7 @@ function ProfileButton({ user }) {
   }, [showMenu]);
   const logout = (e) => {
     e.preventDefault();
+    history.push("/");
     dispatch(sessionActions.logout());
   };
   return (
--- frontend/src/components/Player/index.js
+++ frontend/src/components/Player/index.js
@@ -1,23 +1,28 @@
 import React from "react";
 import ReactAudioPlayer from "react-audio-player";
 import { useSelector } from "react-redux";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 
 const Player = () => {
   const song = useSelector((state) => state.player.song);
 
   return (
-    <>
-      <div>
-        <ReactAudioPlayer
-          src={song.audioUrl}
-          autoPlay={false}
-          controls={true}
-          volume={0.5}
-          muted={false}
-        />
-      </div>
-    </>
+    <div>
+      <AudioPlayer
+        src="https://felipesoundcloudclone.s3.us-west-1.amazonaws.com/StartingOver.mp3"
+        onPlay={(e) => console.log("onPlay")}
+        // other props here
+      />
+    </div>
   );
 };
 
+// const Player = () => (
+//   <AudioPlayer
+//     src="https://felipesoundcloudclone.s3.us-west-1.amazonaws.com/StartingOver.mp3"
+//     onPlay={(e) => console.log("onPlay")}
+//     // other props here
+//   />
+// );
 export default Player;
