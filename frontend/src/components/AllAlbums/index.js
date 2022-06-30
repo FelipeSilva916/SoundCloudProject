9b753cc6cdfe32b9a63343ee41819be820001675
Felipe Silva <felipesilva916@gmail.com>
2022-06-30 10:22:39 -0700

    Create index.js

---
+++ frontend/src/components/AllAlbums/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import "./AllAlbums.css";
+
+const AllAlbums = () => {
+  return <div></div>;
+};
+
+export default AllAlbums;
