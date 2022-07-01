4ea2966123c7b71e00016c316248e7ff16b22f0c
Felipe Silva <felipesilva916@gmail.com>
2022-06-30 18:31:57 -0700

    upload div

--- frontend/src/components/Navigation/Welcome.js
+++ frontend/src/components/Navigation/Welcome.js
@@ -2,14 +2,16 @@ import React from "react";
 
 const Welcome = () => {
   return (
-    <div className="welcome-comp">
-      <p className="welcome-text">Welcome to SoundCloud!</p>
-      <img
-        className="banner-container-welcome"
-        src={require("../../images/Soundcloud.jpeg")}
-        alt="logo"
-      />
-    </div>
+    <>
+      <div className="welcome-comp">
+        <div className="welcome-container">
+          <p className="welcome-text">Welcome to SoundCloud!</p>
+        </div>
+        {/* <div className="welcome-upload-btn">
+          <h1>Upload Songs</h1>
+        </div> */}
+      </div>
+    </>
   );
 };
 
