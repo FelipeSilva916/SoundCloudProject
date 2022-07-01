ef545618cd78f533d1f15bfa374a848616ab17d8
Felipe Silva <felipesilva916@gmail.com>
2022-06-30 17:41:39 -0700

    upload song

---
+++ frontend/src/components/UploadSong/UploadSongForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UploadSongForm = () => {
+  return <div></div>;
+};
+
+export default UploadSongForm;
---
+++ frontend/src/components/UploadSong/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./UploadSong.css";
+
+const UploadSong = () => {
+  return <div></div>;
+};
+
+export default UploadSong;
