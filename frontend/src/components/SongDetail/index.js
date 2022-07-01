9fa9bd378629ea3594b088ff5b56713eb182c032
Felipe Silva <felipesilva916@gmail.com>
2022-07-01 08:56:38 -0700

    cleaned out 'songDetail'

--- frontend/src/components/SongDetail/index.js
+++ frontend/src/components/SongDetail/index.js
@@ -5,60 +5,6 @@ import { getAllSongs } from "../../store/song";
 import { playSong } from "../../store/player";
 import "./SongDetail.css";
 
-const SongDetails = () => {
-  const { songId } = useParams();
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.session.user);
-  //   const songs = useSelector((state) => state.songs);
-  const history = useHistory();
-  const songs = useSelector((state) => Object.values(state.songs));
-
-  useEffect(() => {
-    dispatch(getAllSongs());
-  }, [dispatch, songId]);
-
-  const song = songs[songId];
-  console.log(songs.id);
-  const handleEditBtn = (songId) => {
-    history.push(`/songs/${songId}/edit`);
-  };
-
-  const playSongBtn = useCallback(
-    (song) => {
-      dispatch(playSong(song));
-    },
-    [dispatch]
-  );
-
-  return (
-    <div className="song-portal-lrg">
-      <div>
-        <div className="portal-song-details">
-          <div>
-            <button
-              className="primary-play-btn"
-              onClick={() => playSongBtn(song)}
-            >
-              <i className="fas fa-play"></i>
-            </button>
-            <div>
-              <h2>{song?.title}</h2>
-              <Link
-                className="song-hero-link"
-                to={{ pathname: `/users/${song?.userId}` }}
-              >
-                <h3>{user.username}</h3>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div
-          className="song-img-lrg"
-          style={{ backgroundImage: "url(" + song?.previewImage + ")" }}
-        ></div>
-      </div>
-    </div>
-  );
-};
+const SongDetails = () => {};
 
 export default SongDetails;
