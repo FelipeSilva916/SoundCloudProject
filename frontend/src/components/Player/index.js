0c81b07b7740e98c86b52e82bc4eaf5f2a3c3b1d
Felipe Silva <felipesilva916@gmail.com>
2022-06-30 19:26:30 -0700

    error not showing fixed

--- frontend/src/components/Player/index.js
+++ frontend/src/components/Player/index.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 import "./Player.css";
+
 const Player = () => {
   const song = useSelector((state) => state.player.song);
 
