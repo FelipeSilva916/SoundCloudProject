2d50b6f2f0f67ae3c10387b770227bf61d8cf9f9
Felipe Silva <felipesilva916@gmail.com>
2022-06-30 17:45:57 -0700

    Update UploadSongForm.js

--- frontend/src/components/UploadSong/UploadSongForm.js
+++ frontend/src/components/UploadSong/UploadSongForm.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const UploadSongForm = () => {
+  const sessionUser = useSelector((state) => state.session.user);
+
+  let uploadForm;
+
   return <div></div>;
 };
 
