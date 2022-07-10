edc5b75699acd14b6facb28207951b58f0fb5733
Felipe Silva <felipesilva916@gmail.com>
2022-07-10 15:46:44 -0700

    transparent navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
@@ -8,6 +8,7 @@ import SongDetail from "./components/SongDetail";
 import AllAlbumsComponent from "./components/AllAlbums";
 //
 //
+
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
