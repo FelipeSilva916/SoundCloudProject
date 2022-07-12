57d73f63cbb2f89f65f9934c9920a159b97ad271
Felipe Silva <felipesilva916@gmail.com>
2022-07-11 19:49:47 -0700

    updated

---
+++ frontend/src/components/EditAlbumModal/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EditAlbumModal from "./EditAlbumModal";
+
+const EditAlbumModal = () => {
+  return (
+    <div>
+      <h1>Edit Album</h1>
+    </div>
+  );
+};
+
+export default EditAlbumM;
+odal;
