05dc4bb01c6645e84c0f60c63dd6680f71ce3b7b
Felipe Silva <felipesilva916@gmail.com>
2022-07-14 14:55:47 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
-  const songs = Object.values(useSelector((state) => state.songs));
+  // const songs = Object.values(useSelector((state) => state.songs));
   const albums = Object.values(useSelector((state) => state.albums));
 
   return (
