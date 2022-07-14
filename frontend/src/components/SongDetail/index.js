00e5c74c420b49ee91296a7f108582a592f9a7cb
Felipe Silva <felipesilva916@gmail.com>
2022-07-14 15:52:12 -0700

    play song from detail page

--- frontend/src/components/SongDetail/index.js
+++ frontend/src/components/SongDetail/index.js
@@ -1,8 +1,8 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getSong } from "../../store/song";
-// import { playSong } from "../../store/player";
+import { playSong } from "../../store/player";
 import DeleteSongButton from "../DeleteSongButton";
 import EditSongModal from "../EditSongModal";
 import "./SongDetail.css";
@@ -18,13 +18,13 @@ const SongDetails = () => {
   }, [dispatch, songId]);
 
   const song = songObject[songId];
-
-  // const playSongBtn = useCallback(
-  //   (song) => {
-  //     dispatch(playSong(song));
-  //   },
-  //   [dispatch]
-  // );
+  console.log(song);
+  const playSongBtn = useCallback(
+    (song) => {
+      dispatch(playSong(song));
+    },
+    [dispatch]
+  );
 
   let userManipulateButton;
 
@@ -57,6 +57,15 @@ const SongDetails = () => {
             <h2>Artist: {song?.Artist?.username}</h2>
             <p>Description: {song?.description}</p>
 
+            <div className="play-button-song-detail">
+              <button
+                className="fa-solid fa-circle-play play-btn"
+                onClick={(e) => {
+                  e.preventDefault();
+                  playSongBtn(song);
+                }}
+              ></button>
+            </div>
             <div className="footer-container">
               <div className="song-detail-footer">{userManipulateButton}</div>
             </div>
